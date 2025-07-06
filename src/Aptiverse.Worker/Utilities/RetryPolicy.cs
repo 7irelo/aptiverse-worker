5dbada0659b888ac285378525a651aba71eaaabb
7irelo <tirelo.eric@gmail.com>
2025-07-06 13:51:13 +0200

    Add RetryPolicy class for asynchronous retries
    Introduces a new `RetryPolicy` class in the `Aptiverse.Worker.Utilities` namespace.
    The class features a static method `CreatePolicy` that leverages the Polly library to implement an asynchronous retry policy.
    This policy handles exceptions and retries up to 3 times with an exponential backoff strategy based on `2^retryAttempt` seconds.

---
+++ src/Aptiverse.Worker/Utilities/RetryPolicy.cs
@@ -0,0 +1,13 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace Aptiverse.Worker.Utilities
+{
+    public static class RetryPolicy
+    {
+        public static AsyncRetryPolicy CreatePolicy() =>
+            Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+    }
+}
