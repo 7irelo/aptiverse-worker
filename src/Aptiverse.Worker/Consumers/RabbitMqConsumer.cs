39228d9e7aed9ff6fd52351aa3c0e2be5d01f5a5
7irelo <tirelo.eric@gmail.com>
2025-07-06 13:53:59 +0200

    Add RabbitMqConsumer for message processing
    Introduces the `RabbitMqConsumer` class in the `Aptiverse.Worker.Consumers` namespace. This class connects to RabbitMQ, consumes messages from a specified queue, and processes them using a `MessageProcessor`. The implementation includes message deserialization into `TaskPayload` and supports graceful shutdown via a cancellation token.

---
+++ src/Aptiverse.Worker/Consumers/RabbitMqConsumer.cs
@@ -0,0 +1,50 @@
+﻿using Aptiverse.Worker.Config;
+using Aptiverse.Worker.Models;
+using Aptiverse.Worker.Services;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client.Events;
+
+namespace Aptiverse.Worker.Consumers
+{
+    public class RabbitMqConsumer(IOptions<RabbitMqSettings> config, MessageProcessor processor)
+    {
+        private readonly RabbitMqSettings _settings = config.Value;
+
+        public async Task StartListeningAsync(CancellationToken stoppingToken)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri(_settings.Host),
+                UserName = _settings.Username,
+                Password = _settings.Password
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(_settings.QueueName, true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var payload = JsonSerializer.Deserialize<TaskPayload>(message);
+
+                if (payload != null)
+                    await processor.ProcessAsync(payload);
+
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            channel.BasicConsume(_settings.QueueName, false, consumer);
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(500, stoppingToken);
+            }
+        }
+    }
+}
