6dce34c897d5f1fcf648eb57a7c64d3a74410572
7irelo <tirelo.eric@gmail.com>
2025-07-06 13:52:57 +0200

    Add FastApiClient for API communication
    Introduces the `FastApiClient` class in the `Aptiverse.Worker.Clients` namespace. This class provides methods for sending HTTP POST requests to the FastAPI backend for summarization and emotion analysis. It uses dependency injection to configure the base URL and manage HTTP requests.

---
+++ src/Aptiverse.Worker/Clients/FastApiClient.cs
@@ -0,0 +1,22 @@
+﻿using Aptiverse.Worker.Config;
+using Aptiverse.Worker.Models;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Json;
+
+namespace Aptiverse.Worker.Clients
+{
+    public class FastApiClient(HttpClient client, IOptions<FastApiSettings> settings)
+    {
+        private readonly string _baseUrl = settings.Value.BaseUrl;
+
+        public async Task<HttpResponseMessage> SendToSummarizer(TaskPayload payload)
+        {
+            return await client.PostAsJsonAsync($"{_baseUrl}/summarize", payload);
+        }
+
+        public async Task<HttpResponseMessage> SendToEmotionAnalysis(TaskPayload payload)
+        {
+            return await client.PostAsJsonAsync($"{_baseUrl}/emotion-analysis", payload);
+        }
+    }
+}
